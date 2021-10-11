2f80629d4ed19566af00a25e8d144be857ea9e38
Pavel Korsakov <django@code.travel>
2021-10-12 05:58:36 +0700

    adding pystun3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import glob
 import os
 from string import ascii_uppercase
+# import http.client
+import stun
 
 
 def check_environment():
@@ -9,8 +11,18 @@ def check_environment():
             while line := f.readline():
                 if line[0] in ascii_uppercase:
                     if not os.getenv(f"{line.split('=')[0].strip()}"):
-                        raise Exception(f"{line.split('=')[0].strip()} is None")
-                        # print(f"{line.split('=')[0].strip()} is None")
+                        # Внешний IP средствами python
+                        # conn = http.client.HTTPConnection("ifconfig.me")
+                        # conn.request("GET", "/ip")
+                        # ip = conn.getresponse().read()
+                        # error_message = f"{line.split('=')[0].strip()} is None"
+
+                        # Внешний IP через библиотеку pystun3
+                        nat_type, external_ip, external_port = stun.get_ip_info()
+                        error_message = f"{line.split('=')[0].strip()} is None. nat_type: {nat_type}, external_ip: {external_ip}, external_port={external_port}"
+
+                        raise Exception(error_message)
+                        # print(error_message)
 
 
 if __name__ == '__main__':
